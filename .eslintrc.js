0d684251b1e7fa9dd246efd3f221c895c5fe91a1
Kacper Zabielski <beelzick.dev@gmail.com>
2024-09-14 14:08:26 +0200

    Extend eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,80 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  root: true,
+  parser: '@typescript-eslint/parser',
   ignorePatterns: ['dist'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'unicorn', 'unused-imports', 'no-relative-import-paths'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'next/typescript',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  rules: {
+    'jsx-a11y/media-has-caption': 'off',
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'consistent-return': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          { pattern: 'react*', group: 'builtin', position: 'before' },
+          { pattern: '@/**', group: 'external', position: 'after' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'newline-before-return': 'error',
+    // 'no-restricted-syntax': 'off',
+    'no-relative-import-paths/no-relative-import-paths': [
+      'error',
+      { allowSameFolder: true, prefix: '@', rootDir: 'src' },
+    ],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+    ],
+    'react/hook-use-state': 'error',
+    'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    // 'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        multiline: 'last',
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandFirst: true,
+        shorthandLast: false,
+      },
+    ],
+    'react/no-danger': 'off',
+    'react/require-default-props': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
 };
