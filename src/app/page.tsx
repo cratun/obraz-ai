145900e4ab000d5190ff1c9117c1731a7ade9a56
Przemek Paziewski <ppaazz56@gmail.com>
2024-09-21 14:20:17 +0200

    refactor(home): text changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,22 @@ import AppButton from './_components/app-button';
 import AppContainer from './_components/app-container';
 import AppLogo from './_components/app-logo';
 import HomeSwiper from './_components/home-swiper';
+
 const generateSteps = [
   {
     title: 'Opisz swój pomysł',
-    description: 'Im bardziej szczegółowo opiszesz, tym lepszy rezultat uzyskasz.',
+    description: 'Podziel się z nami swoją wizją. Im więcej szczegółów, tym bardziej wyjątkowy będzie Twój obraz.',
     Icon: BulbIcon,
   },
   {
     title: 'Wybierz styl',
-    description: 'Przygotowaliśmy dla Ciebie style, abyś w prosty sposób mógł wybrać.',
+    description:
+      'Wybierz spośród naszych starannie dobranych stylów, aby nadać swojemu dziełu niepowtarzalny charakter.',
     Icon: StyleIcon,
   },
   {
     title: 'Kup i zapłać',
-    description: 'Nie musisz zakładać konta, kliknij kup i jednym kliknięcie kup obraz.',
+    description: 'Bez zakładania konta! Kliknij "Kup teraz" i sfinalizuj zamówienie w kilka chwil.',
     Icon: CardIcon,
   },
 ];
@@ -46,7 +48,7 @@ const GenerateStep = ({
           'absolute left-1/2 top-1/2 h-2 -translate-y-1/2 border-y border-[#57C6A3] bg-primary',
           index === 0 && 'w-[50vw]',
           index && index !== 0 && 'w-screen -translate-x-1/2',
-          !index && 'w-[50vw] -translate-x-[100%]',
+          index === undefined && 'w-[50vw] -translate-x-[100%]',
         )}
       />
       <div className="relative z-10 flex aspect-square w-24 items-center justify-center rounded-full border border-[#57C6A3] bg-[#41BE96]">
@@ -54,7 +56,7 @@ const GenerateStep = ({
       </div>
     </div>
     <div className="flex flex-col gap-2.5">
-      <h4 className="tracking-[1px text-xl font-semibold leading-[120%]">{title}</h4>
+      <h4 className="text-xl font-semibold leading-[120%] tracking-[1px]">{title}</h4>
       <p className="leading-[150%] tracking-[0.5px]">{description}</p>
     </div>
   </div>
@@ -68,13 +70,14 @@ const Home = () => {
           <div className="flex flex-col gap-10 lg:max-w-[400px]">
             <AppLogo />
             <div className="flex flex-col gap-10 lg:my-auto">
-              <h1 className="text-6xl font-bold leading-[120%] tracking-[1px]">
-                <span className="text-primary">Wydrukuj</span> obraz z wyobraźni<span className="text-primary">.</span>
+              <h1 className="text-5xl font-bold leading-[120%] tracking-[1px]">
+                <span className="text-primary">Ożyw</span> swoje marzenia na płótnie
+                <span className="text-primary">!</span>
               </h1>
               <div className="h-10 w-full bg-text"></div>
               <p className="leading-[150%] tracking-[0.5px]">
-                Unikalny obraz ożywiający pomieszczenie, idealny pomysł na prezent, poczuj sie jak artysta. Wygeneruj
-                swój obraz i wydrukuj już dziś.
+                Stwórz niepowtarzalny obraz, który zachwyci każdego. Idealny pomysł na prezent! Poczuj się jak artysta i
+                wygeneruj swój własny obraz na płótnie. Zamów już dziś!
               </p>
             </div>
             <span className="hidden lg:block">
@@ -111,7 +114,7 @@ const Home = () => {
               </GenerateStep>
             ))}
             <GenerateStep
-              description="Nie musisz zakładać konta, kliknij kup i jedym obraz, asdfff"
+              description="Otrzymaj swój wyjątkowy obraz. Wydrukujemy go na płótnie najwyższej jakości i dostarczymy prosto do Ciebie."
               title="Obraz jest Twój"
             >
               <Image
@@ -128,8 +131,12 @@ const Home = () => {
       <AppContainer className="py-10">
         <AppContainer.Content className="flex-col gap-10 text-text">
           <div className="flex flex-col gap-2.5">
-            <h4 className="text-lg leading-[140%] tracking-[0.5px] text-primary">Ponad 1000 zadowolonych klientów</h4>
-            <h2 className="text-3xl font-semibold leading-[120%] tracking-[1px]">Odkryj inspirujące projekty</h2>
+            <h4 className="text-lg leading-[140%] tracking-[0.5px] text-primary">
+              Dołącz do grona ponad 1000 zadowolonych klientów
+            </h4>
+            <h2 className="text-3xl font-semibold leading-[120%] tracking-[1px]">
+              Zainspiruj się naszymi wyjątkowymi realizacjami
+            </h2>
           </div>
         </AppContainer.Content>
       </AppContainer>
@@ -142,13 +149,13 @@ const Home = () => {
             LinkComponent={Link}
             variant="contained"
           >
-            Przejdź do generatora
+            Stwórz swój obraz teraz
           </AppButton>
         </AppContainer.Content>
       </AppContainer>
       <AppContainer className="py-10">
         <AppContainer.Content className="flex flex-col gap-10">
-          <h2 className="text-3xl font-semibold leading-[120%] tracking-[1px]">Nasze obrazy są wysokiej jakości</h2>
+          <h2 className="text-3xl font-semibold leading-[120%] tracking-[1px]">Najwyższa jakość gwarantowana</h2>
           <div className="flex flex-col gap-10 md:flex-row">
             <Image
               alt="Przykładowy obraz"
@@ -168,16 +175,18 @@ const Home = () => {
             />
             <div className="flex flex-col gap-5">
               <p className="max-w-md leading-[150%] tracking-[0.5px]">
-                Unikalny obraz ożywiający pomieszczenie, idealny pomysł na prezent, poczuj sie jak artysta. Wygeneruj
-                swój obraz i wydrukuj już dziś.
+                Każdy obraz to połączenie Twojej wyobraźni i naszej pasji do jakości. Ożyw swoje wnętrza i zaskocz
+                bliskich niepowtarzalnym prezentem. Stwórz i zamów swój obraz już dziś!
               </p>
               <AppButton
-                classes={{ contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5 w-fit' }}
                 href="/generate"
                 LinkComponent={Link}
                 variant="contained"
+                classes={{
+                  contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5 w-fit',
+                }}
               >
-                Przejdź do generatora
+                Stwórz swój obraz teraz
               </AppButton>
             </div>
           </div>
