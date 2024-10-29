7dc090f41139dfdaa74dba10b41f2379dc03f960
Przemek Paziewski <ppaazz56@gmail.com>
2024-10-29 23:23:50 +0100

    feat: add x pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,14 @@ fbq('init', '2576537089212905');
 fbq('track', 'PageView');`}
           </Script>
         )}
+        {process.env.NODE_ENV !== 'development' && (
+          <Script id="x-pixel" strategy="afterInteractive">
+            {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','opn45');`}
+          </Script>
+        )}
       </body>
     </html>
   );
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import Script from 'next/script';
 import AppButton from '@/app/_components/app-button';
 import AppContainer from '@/app/_components/app-container';
 import { CONTACT_EMAIL } from '@/app/_utils/constants';
@@ -19,76 +20,88 @@ const PageSuccess = async ({ searchParams }: { searchParams: SerachParams }) => 
   const { customer, session } = await actionGetOrderInfo(searchParams.session_id as string);
 
   return (
-    <AppContainer className="pt-20">
-      <div className="absolute right-0 top-0 -z-10 hidden h-screen w-full max-w-sm overflow-hidden bg-primary lg:block xl:max-w-md 2xl:max-w-xl">
-        <LogoIcon />
-      </div>
-      <AppContainer.Content>
-        <div className="flex w-fit max-w-xl flex-col gap-10 py-5">
-          <h1 className="text-3xl font-semibold leading-[120%] tracking-[1px]">
-            <span className="text-primary">{customer.name}</span>, dziękujemy za zamówienie.
-          </h1>
-          <p className="leading-[150%] tracking-[0.5px]">
-            Szczegóły Twojego zamówienia:
-            <br />
-            <br />
-            {typeof session.payment_intent === 'string' && (
-              <>
-                <strong>Numer zamówienia:</strong> {session.payment_intent}
-              </>
-            )}
-            <br />
-            <br />
-            {session.metadata && (
-              <>
-                <strong>Rozmiar obrazu:</strong> {session.metadata.size}x{session.metadata.size} cm
-              </>
-            )}
-            <br />
-            <br />
-            <strong>Wartość:</strong> {(session.amount_total || 0) / 100} zł
-            <br />
-            <br />
-            <strong>Adres wysyłki: </strong> {customer.address?.city}, {customer.address?.line1},
-            {customer.address?.line2 && <>{customer.address?.line2}, </>}
-            {customer.address?.postal_code}, {customer.address?.country}
-            <br />
-            <br />
-            <strong>Adres email:</strong> {customer.email}
-            <br />
-            <br />
-            <strong>Oczekiwany czas realizacji zamówienia: 3 - 5 dni roboczych.</strong>
-            <br />
-            <br />
-            Jeżeli masz jakiekolwiek pytania lub dane powyżej są niepoprawne skontaktuj się z nami pod adresem{' '}
-            <strong>{CONTACT_EMAIL}</strong>
-            <br />
-            <br />
-            Na adres email podany w zamówieniu zostały przesłane szczegóły. <br />
-            <br />
-            Dziękujemy za skorzystanie z ObrazAi. Zachęcamy do ponownego zakupu.
-          </p>
-          <div className="flex flex-wrap gap-2.5">
-            <AppButton
-              classes={{ contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5' }}
-              href="/generate"
-              LinkComponent={Link}
-              variant="contained"
-            >
-              Stwórz nowy obraz
-            </AppButton>
-            <AppButton
-              classes={{ contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5' }}
-              href="/"
-              LinkComponent={Link}
-              variant="outlined"
-            >
-              Wróć do strony głównej
-            </AppButton>
-          </div>
+    <>
+      <AppContainer className="pt-20">
+        <div className="absolute right-0 top-0 -z-10 hidden h-screen w-full max-w-sm overflow-hidden bg-primary lg:block xl:max-w-md 2xl:max-w-xl">
+          <LogoIcon />
         </div>
-      </AppContainer.Content>
-    </AppContainer>
+        <AppContainer.Content>
+          <div className="flex w-fit max-w-xl flex-col gap-10 py-5">
+            <h1 className="text-3xl font-semibold leading-[120%] tracking-[1px]">
+              <span className="text-primary">{customer.name}</span>, dziękujemy za zamówienie.
+            </h1>
+            <p className="leading-[150%] tracking-[0.5px]">
+              Szczegóły Twojego zamówienia:
+              <br />
+              <br />
+              {typeof session.payment_intent === 'string' && (
+                <>
+                  <strong>Numer zamówienia:</strong> {session.payment_intent}
+                </>
+              )}
+              <br />
+              <br />
+              {session.metadata && (
+                <>
+                  <strong>Rozmiar obrazu:</strong> {session.metadata.size}x{session.metadata.size} cm
+                </>
+              )}
+              <br />
+              <br />
+              <strong>Wartość:</strong> {(session.amount_total || 0) / 100} zł
+              <br />
+              <br />
+              <strong>Adres wysyłki: </strong> {customer.address?.city}, {customer.address?.line1},
+              {customer.address?.line2 && <>{customer.address?.line2}, </>}
+              {customer.address?.postal_code}, {customer.address?.country}
+              <br />
+              <br />
+              <strong>Adres email:</strong> {customer.email}
+              <br />
+              <br />
+              <strong>Oczekiwany czas realizacji zamówienia: 3 - 5 dni roboczych.</strong>
+              <br />
+              <br />
+              Jeżeli masz jakiekolwiek pytania lub dane powyżej są niepoprawne skontaktuj się z nami pod adresem{' '}
+              <strong>{CONTACT_EMAIL}</strong>
+              <br />
+              <br />
+              Na adres email podany w zamówieniu zostały przesłane szczegóły. <br />
+              <br />
+              Dziękujemy za skorzystanie z ObrazAi. Zachęcamy do ponownego zakupu.
+            </p>
+            <div className="flex flex-wrap gap-2.5">
+              <AppButton
+                classes={{ contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5' }}
+                href="/generate"
+                LinkComponent={Link}
+                variant="contained"
+              >
+                Stwórz nowy obraz
+              </AppButton>
+              <AppButton
+                classes={{ contained: 'normal-case font-normal leading-[150%] tracking-[0.5px] px-5 py-2.5' }}
+                href="/"
+                LinkComponent={Link}
+                variant="outlined"
+              >
+                Wróć do strony głównej
+              </AppButton>
+            </div>
+          </div>
+        </AppContainer.Content>
+      </AppContainer>
+      {session && customer && process.env.NODE_ENV !== 'development' && (
+        <Script id="x-pixel-success" strategy="lazyOnload">
+          {`twq('event', 'tw-opn45-opn73', {
+    value: ${(session.amount_total || 0) / 100},
+    currency: 'PLN',
+    email_address: '${customer.email}',
+    phone_number: ${customer.phone},
+  });`}
+        </Script>
+      )}
+    </>
   );
 };
 
