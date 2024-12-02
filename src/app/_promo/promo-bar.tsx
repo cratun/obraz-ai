6613b499ed2158a7d455cec7f3f740ec0b34d080
Kacper Zabielski <beelzick.dev@gmail.com>
2024-12-02 19:11:05 +0100

    cyber monday color fix

--- src/app/_promo/promo-bar.tsx
+++ src/app/_promo/promo-bar.tsx
@@ -24,7 +24,7 @@ const PromoBar = () => {
   }
 
   return (
-    <div className="flex min-h-[17px] items-center justify-center bg-[#361D2E]/80 px-5">
+    <div className="flex min-h-[17px] items-center justify-center bg-[#6D41A1] px-5">
       <AppContainer.Content className="block text-xs text-white">
         <div>
           <strong>-{PROMO_PERCENTAGE_VALUE}%</strong> z kodem <strong className="font-sans">{PROMO_CODE}</strong> |
