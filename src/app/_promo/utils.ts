492846ea297d98c6208e46f813caf4e68ca46790
Kacper Zabielski <86677657+beelzick@users.noreply.github.com>
2024-12-03 09:22:15 +0100

    gift promo 22 (#25)

--- src/app/_promo/utils.ts
+++ src/app/_promo/utils.ts
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
 
-export const PROMO_END_DATE = dayjs('2024-12-02').endOf('day');
-export const PROMO_CODE = 'CYBER';
-export const PROMO_PERCENTAGE_VALUE = 30;
+export const PROMO_END_DATE = dayjs('2024-12-05').endOf('day');
+export const PROMO_CODE = 'PREZENT22';
+export const PROMO_PERCENTAGE_VALUE = 22;
 
 export const getIsPromoExpired = () => {
   const end = dayjs(PROMO_END_DATE);
@@ -13,4 +13,4 @@ export const getIsPromoExpired = () => {
 
 export const SPECIAL_PROMO_COOKIE = '8CfUgS-ge1RBcWWPSxssz';
 
-export const IS_SPECIAL_PROMO_ENABLED = false;
+export const IS_SPECIAL_PROMO_ENABLED = true;
